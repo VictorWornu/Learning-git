e412d50bcc6fdb88c85fd00981f9d0f540a8cbbf
VictorWornu <wornuvictor@gmail.com>
2020-05-29 22:50:12 +0100

    This is the first reasonable thing I've done with git

---
+++ just checking.py
@@ -0,0 +1,37 @@
+#s = "Look over that way"
+#print( s.find("over") )
+
+#name = "hmu john gn"
+#print( name.strip("hmu") )
+
+#num = 4.24
+#print(type(num))
+
+#g = [1, 2, 3]
+#print(id(g))
+
+#for num in range(5):
+ #     if num == 3:
+  #            break
+   #   print(num)
+
+#health = 10
+#while health > 0:
+      #print(health)
+      #health == "green" 
+      #break
+
+#def sayHello ():
+#	sayHello
+
+#def calc( ):
+  #    x, y = 5, 10
+ #     print(x + y)
+#calc( )
+
+
+def printName(full_name):
+      print( "Your name is: { }".format(full_name) )
+printName("John Smith")
+printName("Amanda")
+printName()
